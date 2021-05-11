45864d6177a1e3d4bcfeb274bf123ef938432226
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-05-11 05:27:05 +0430

    add cs output to statistics, add statistics to report

--- src/constants.py
+++ src/constants.py
@@ -4,5 +4,6 @@ import os
 BASE_DATA_URL =  "data" 
 RAW_DATA_DIR = os.path.join(BASE_DATA_URL, 'raw') 
 PREPROCCESSED_DATA_DIR = os.path.join(BASE_DATA_URL, 'preprocessed') 
+STATISTICS_BASE_DIR = os.path.join(PREPROCCESSED_DATA_DIR, 'Statistics') 
 
 CLASSES_DATA_URL = BASE_DATA_URL + "/classification_data/classes_data.csv"
