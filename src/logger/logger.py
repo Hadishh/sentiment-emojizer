58da5d3d0ee33fac28579820c41eda9d65edec8a
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-06-27 15:29:59 +0430

    train models again, add logs, update doc

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,4 +1,4 @@
-from src.constants import WORD2VEC_LOG_FILE
+from src.constants import WORD2VEC_LOG_FILE, LANGUAGE_MODEL_LOG_FILE
 import os
 import datetime
 def log(message, task):
@@ -8,4 +8,11 @@ def log(message, task):
                 f.write(f"[{datetime.datetime.now()}] {message}\n")
         else:
             with open(WORD2VEC_LOG_FILE, 'a') as f:
+                f.write(f"[{datetime.datetime.now()}] {message}\n")
+    if(task == "language_model"):
+        if (not os.path.exists(LANGUAGE_MODEL_LOG_FILE)):
+            with open(LANGUAGE_MODEL_LOG_FILE, 'w') as f:
+                f.write(f"[{datetime.datetime.now()}] {message}\n")
+        else:
+            with open(LANGUAGE_MODEL_LOG_FILE, 'a') as f:
                 f.write(f"[{datetime.datetime.now()}] {message}\n")
